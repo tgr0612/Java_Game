28cf0837a3614f5411d69f51a9aa2cf59ab0e53a
Administrator <Administrator@114.70.235.137>
2016-11-03 19:13:17 +0900

    웹뷰셋팅

---
+++ example/src/com/example/example/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.example;
+import android.webkit.WebView;
+
+import android.webkit.WebViewClient;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+	private WebView mWebView;    // 웹뷰 선언
+	   @Override 
+
+	    public void onCreate(Bundle savedInstanceState) {
+
+	        super.onCreate(savedInstanceState);
+
+	        setContentView(R.layout.activity_main);
+
+	        
+
+	        mWebView = (WebView) findViewById(R.id.WebView1);
+
+	        mWebView.getSettings().setJavaScriptEnabled(true);      // 웹뷰에서 자바 스크립트 사용
+
+	        mWebView.loadUrl( "192.168.0.1" );            // 웹뷰에서 불러올 URL 입력
+
+	        mWebView.setWebViewClient(new WishWebViewClient());
+
+	    }
+	   @Override
+
+	    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+	    	if((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()){
+
+	    		mWebView.goBack();
+
+	    		return true;    		
+
+	    	}
+
+	        return super.onKeyDown(keyCode, event);
+
+	    }
+
+
+
+	    private class WishWebViewClient extends WebViewClient{
+
+	    	@Override
+
+	    	public boolean shouldOverrideUrlLoading(WebView view, String url){
+
+	    		view.loadUrl(url);
+
+	    		return true;
+
+	    	}
+
+	    }
+
+
+}
